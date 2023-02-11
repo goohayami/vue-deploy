eb4a93134165e483d62d7da9de013deffb85e9fb
goohayami <goohayami@gmail.com>
2023-02-11 14:42:29 +0900

    s

---
+++ practoce208/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+
+</script>
+
+<template> 
+  <main>
+    <HelloWorld />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ practoce208/src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <h2><span>This is Vue.js lecture 75</span></h2>
+  <div v-for="name in names" key="name">{{ name }}</div>
+
+  <input type="text" v-model="ga">
+  <p>{{ ga }}</p>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  name: "Home",
+  setup() {
+    const names = ref(["mario", "luiji", "koopa"]);
+
+    return { names };
+  },
+};
+</script>
+
+<style scoped>
+span {
+  color: red;
+}
+</style>
